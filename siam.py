0a1c6c5a315018591d8271c349376990be146f73
Max Cohen <max.cohen@gmx.com>
2018-01-31 10:13:34 +0100

    readability

--- siam.py
+++ siam.py
@@ -29,12 +29,12 @@ def siamRD(model_data,
 
     # Train
     if b_train_model:
-        batch_size = model_data["batch_size"]
-        steps_per_epoch = model_data["steps_per_epoch"]
-        epochs = model_data["epochs"]
-        validation_steps = model_data["validation_steps"]
 
-        histo = train_model(model, dataset=model_data["dataset_path"], batch_size=batch_size, steps_per_epoch=steps_per_epoch, epochs=epochs, validation_steps=validation_steps)
+        histo = train_model(model,
+                                dataset=model_data["dataset_path"], batch_size=model_data["batch_size"],
+                                steps_per_epoch=model_data["steps_per_epoch"],
+                                epochs=model_data["epochs"],
+                                validation_steps=model_data["validation_steps"])
 
     # Test
     if b_test_model:
