93827a3fd3fb6fe5c8359362c59fe68e682b0695
Max Cohen <max.cohen@gmx.com>
2018-02-18 18:10:28 +0100

    [minor] updated log function

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,6 +1,9 @@
 def log(msg, type="normal"):
     if type == "error":
         print("\033[1m\033[91mERROR\033[0m", end=" ")
+    elif type == "info":
+        print("\033[1m\033[92mINFO \033[0m", end=" ")
     elif type == "normal":
         print("\033[1m\033[94mLOG  \033[0m", end=" ")
+
     print(msg)
